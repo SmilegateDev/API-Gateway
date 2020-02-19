adcc0b26b42ea1310f816cf831593c6ad3dae8ea
LaVi <lastvirgo266@gmail.com>
2020-02-19 13:27:36 +0900

    Gateway Test Success

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const httpProxy = require('express-http-proxy');
 const userServiceProxy = httpProxy('https://127.0.0.1:8005/test');
 const url = require('url');
+const request = require('request');
 
 const { verifyToken, apiLimiter } = require('./middlewares');
 
@@ -11,13 +12,17 @@ const router = express.Router();
 
 
 
-// router.get('/token', (req, res, next)=>{
-//    userServiceProxy(req, res, next);
-// })
+router.get('/token', (req, res, next)=>{
+   request({
+       method : 'GET',
+       uri : 'http://localhost:8005/test' 
+   },function(err, response, body){
+        if(err){throw err;}
+        return res.status(200).send(body);
+   });
+});
 
-express().use('/token', userServiceProxy);
-
-
+// express().use('/token', userServiceProxy);
 
 router.get('/test', (req, res, next)=>{
     return res.status(200).json({
